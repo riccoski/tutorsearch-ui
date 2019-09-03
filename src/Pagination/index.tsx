b0727a5d5e90dd9bf0843ceef7959068faa5a503
Ricco Sobers <riccosobers@gmail.com>
2019-09-03 15:07:12 +0100

    fix pages showing in pagination

--- src/Pagination/index.tsx
+++ src/Pagination/index.tsx
@@ -15,7 +15,7 @@ const Pagination: React.FC<{
     if (onPageChange) onPageChange(pageNumber);
   };
 
-  const pages = [new Array(totalPages).keys()].map((_, index) => index + 1);
+  const pages = Array.from(Array(10).keys()).map((_, index) => index + 1);
 
   return (
     <div className={styles.pagination}>
