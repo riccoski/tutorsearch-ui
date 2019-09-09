8d3bd0c606b7c58de80bb50804e15aacb72671aa
Ricco Sobers <riccosobers@gmail.com>
2019-09-09 20:37:29 +0100

    Form composition

--- src/Form/Form.tsx
+++ src/Form/Form.tsx
@@ -1,28 +1,31 @@
 import * as React from "react";
 import classNames from "classnames";
-
-import FormActions, { FormActionsProps } from "./FormActions";
-import FormButton from "./FormButton";
-import FormError from "./FormError";
-import FormField from "./FormField";
-import FormInput, { FormInputProps } from "./FormInput";
-import FormTextArea from "./FormTextArea";
 import styles from "./Form.module.scss";
+import { FormActionsProps } from "./FormActions";
+import { FormInputProps } from "./FormInput";
 
-type Form<T> = React.FC<T> & {
+interface FormComposition {
   Actions?: React.FC<FormActionsProps>;
   Button?: React.FC;
   Error?: React.FC;
   Field?: React.FC;
   Input?: React.FC<FormInputProps>;
   TextArea?: React.FC;
-};
+}
 
-const Form: Form<{
+interface Props {
   action?: string;
   className?: string;
   onSubmit?: (e?: React.FormEvent<HTMLFormElement>) => void;
-}> = ({ action, children, className, onSubmit, ...rest }) => {
+}
+
+const Form: React.FC<Props> & FormComposition = ({
+  action,
+  children,
+  className,
+  onSubmit,
+  ...rest
+}) => {
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     if (typeof action !== "string") e.preventDefault();
     if (typeof onSubmit === "function") onSubmit(e);
@@ -37,14 +40,5 @@ const Form: Form<{
     </form>
   );
 };
-
-Form.displayName = "Form";
-
-Form.Actions = FormActions;
-Form.Button = FormButton;
-Form.Error = FormError;
-Form.Field = FormField;
-Form.Input = FormInput;
-Form.TextArea = FormTextArea;
 
 export default Form;
--- src/Form/index.ts
+++ src/Form/index.ts
@@ -1,3 +1,18 @@
 import Form from "./Form";
+import FormActions from "./FormActions";
+import FormButton from "./FormButton";
+import FormError from "./FormError";
+import FormField from "./FormField";
+import FormInput from "./FormInput";
+import FormTextArea from "./FormTextArea";
+
+Form.displayName = "Form";
+
+Form.Actions = FormActions;
+Form.Button = FormButton;
+Form.Error = FormError;
+Form.Field = FormField;
+Form.Input = FormInput;
+Form.TextArea = FormTextArea;
 
 export default Form;
